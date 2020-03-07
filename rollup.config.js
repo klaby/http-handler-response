8066c0f15b5eabc65f9d3797c08fa05996c97f79
hiukky <developermarsh@gmail.com>
2020-03-07 14:34:02 -0300

    chore: update eslint config

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.ts',
+  input: 'index.ts',
   output: [
     {
       file: pkg.main,
